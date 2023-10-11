37316949197fa30112ff304606aa1a9f45030be1
n4m-ward <gabrielphp00@gmail.com>
2023-10-11 14:16:14 -0300

    feat: adicionando tradução na sessão de descrição de carreira

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,20 +1,24 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import { messages as resources } from './locales/index';
+import {messages as resources} from './locales/index';
 
 i18n.use(LanguageDetector).init({
-  resources,
-  order: [localStorage],
-  lookupLocalStorage: 'language',
-  fallbackLng: 'en',
-  debug: false,
-  defaultNS: ['translations'],
-  ns: 'translations',
+    resources,
+    order: [localStorage],
+    lookupLocalStorage: 'language',
+    fallbackLng: 'en',
+    debug: false,
+    defaultNS: ['translations'],
+    ns: 'translations',
 
-  interpolation: {
-    escapeValue: false,
-  },
+    interpolation: {
+        escapeValue: false,
+    },
 });
 
-const { t } = i18n;
-export { i18n, t as translate };
+const {t} = i18n;
+export {i18n};
+
+export function translate(key): string {
+    return t(key)
+}
--- src/translate/locales/en/index.js
+++ src/translate/locales/en/index.js
@@ -1,18 +1,22 @@
 export const en = {
-  translations: {
-    navBar: {
-      navBarLogo: '<GabrielDev />',
-      aboutMe: 'About me',
-      projects: 'Projects',
-      contact: 'Contact me',
+    translations: {
+        navBar: {
+            navBarLogo: '<GabrielDev />',
+            aboutMe: 'About me',
+            projects: 'Projects',
+            contact: 'Contact me',
+        },
+        languageSelector: {
+            portuguese: 'Portuguese',
+            english: 'English',
+        },
+        wellcomePage: {
+            getCv: 'GET CV',
+            contact: 'Contact Me',
+        },
+        carer: {
+            introductionText: "I'm passionate about technology. I had my first experience with programming in 2013 after creating and maintaining Tibia servers using Lua and PHP for a little over a year.",
+            carerDescription: 'In 2020, I obtained my first professional experience, which I continue to pursue to this day, as a mid-level full-stack developer. Along the way, I\'ve been in contact with various technologies, including PHP, Laravel, Node.js, Python, React, Vue.js, RabbitMQ, among others.'
+        }
     },
-    languageSelector: {
-      portuguese: 'Portuguese',
-      english: 'English',
-    },
-    wellcomePage: {
-      getCv: 'GET CV',
-      contact: 'Contact Me',
-    },
-  },
 };
