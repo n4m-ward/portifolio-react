6f40d060f8fcbd48764d110d63289bdf24009d03
n4m-ward <gabrielphp00@gmail.com>
2023-10-11 14:16:14 -0300

    feat: adicionando tradução na sessão de descrição de carreira

--- src/translate/locales/pt/index.js
+++ src/translate/locales/pt/index.js
@@ -1,18 +1,23 @@
 export const pt = {
-  translations: {
-    navBar: {
-      navBarLogo: '<GabrielDev />',
-      aboutMe: 'Sobre Mim',
-      projects: 'Projetos',
-      contact: 'Contato',
+    translations: {
+        navBar: {
+            navBarLogo: '<GabrielDev />',
+            aboutMe: 'Sobre Mim',
+            projects: 'Projetos',
+            contact: 'Contato',
+        },
+        languageSelector: {
+            portuguese: 'Portugues',
+            english: 'Ingles',
+        },
+        wellcomePage: {
+            getCv: 'Curriculo',
+            contact: 'Contato',
+        },
+        carer: {
+            introductionText: 'Sou apaixonado por tecnologia, tive meu primeiro acesso a programação em 2013 apos criar e dar manutenção em servidores de tibia feitos em lua e PHP durante pouco mais de um ano',
+            carerDescription: 'Em 2020 consegui minha primeira experiencia profissional onde sigo até hoje, como desenvolvedor fullstack pleno, após ter passado em contato com varias tecnologias, como: PHP, Laravel, Node.js, Python, React, Vue.js, RabbitMQ, dentre outros.'
+        }
     },
-    languageSelector: {
-      portuguese: 'Portugues',
-      english: 'Ingles',
-    },
-    wellcomePage: {
-      getCv: 'Curriculo',
-      contact: 'Contato',
-    },
-  },
+
 };
